a400b3fe0fa5cba8ad2a9105a6d2a0bb89a2b014
beatmil <anutorn@gmail.com>
2021-08-27 19:41:00 +0700

    Create player template

--- script/elesis.gd
+++ script/elesis.gd
@@ -24,46 +24,54 @@ var is_running_left = false
 # hitbox
 onready var hitbox01 = load("res://prefabs/hitboxes/hitbox01.tscn")
 
+# stun helper
+export var stun01 = false
+var stun_right = false
+var stun_left = false
+var stun_power_right = Vector2(2000,-900)
+var stun_power_left = Vector2(-2000,-900)
+
 func _ready():
 	pass # Replace with function body.
 
 
 func _physics_process(_delta):
 	# normal attack
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("p1_attack"):
+		print("attack01 played")
 		$AnimationPlayer.play("attack01")
 
 
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("p1_up"):
 		if is_on_floor():
 			motion.y -= JUMP_POWER
 
 	# stop moving when both right and left are pressed
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("p1_right") and Input.is_action_pressed("p1_left"):
 		motion.x = 0
-	elif Input.is_action_just_released("ui_right"):
+	elif Input.is_action_just_released("p1_right"):
 		is_running = false
-	elif Input.is_action_just_released("ui_left"):
+	elif Input.is_action_just_released("p1_left"):
 		is_running = false
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("p1_right"):
 		if is_running == true:
 			motion.x = RUN_SPEED
 		else:
 			motion.x = WALK_SPEED
 		$Sprite.set_flip_h(false)
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("p1_left"):
 		if is_running == true:
 			motion.x = -RUN_SPEED
 		else:
 			motion.x = -WALK_SPEED
 		$Sprite.set_flip_h(true)
-	elif Input.is_action_pressed("ui_right") == false:
+	elif Input.is_action_pressed("p1_right") == false:
 		motion.x = lerp(motion.x, 0, FRICTION)
 	else:
 		motion.x = 0
 
 	# check dash right
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("p1_right"):
 		if dash_trigger_right == false:
 			$dash_trigger_right_timer.start()
 			dash_trigger_right = true
@@ -75,7 +83,7 @@ func _physics_process(_delta):
 			is_dashing_right = true
 
 	# check dash left
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_just_pressed("p1_left"):
 		if dash_trigger_left == false:
 			$dash_trigger_left_timer.start()
 			dash_trigger_left = true
@@ -88,14 +96,17 @@ func _physics_process(_delta):
 
 	motion.y += GRAVITY
 
-	if is_dashing_right == true:
+	if stun01 == true:
+		if stun_right == true:
+			motion = stun_power_right
+		elif stun_left == true:
+			motion = stun_power_left
+	elif is_dashing_right == true:
 		motion.x = DASH
-		motion = move_and_slide(motion, UP)
 	elif is_dashing_left == true:
 		motion.x = -DASH
-		motion = move_and_slide(motion, UP)
-	else:
-		motion = move_and_slide(motion, UP)
+
+	motion = move_and_slide(motion, UP)
 
 
 func _on_dash_trigger_left_timer_timeout():
@@ -130,3 +141,20 @@ func attack01():
 	var hitbox01_real = hitbox01.instance()
 	hitbox01_real.position += offset
 	add_child(hitbox01_real) # spawn hitbox
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("attack"):
+		# $hp_bar.decrease_hp(1)
+		$stun01_timer.start()
+		stun01 = true
+		print("stun01 true")
+		if $LRayCast2D.is_colliding():
+			stun_right = true
+		if $RRayCast2D.is_colliding():
+			stun_left = true
+
+
+func _on_stun01_timer_timeout():
+	stun01 = false
+	print("stun01 false")
