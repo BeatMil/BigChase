12e5ef7f92d54dc6363aa8a7f2459d9f2e25870f
beatmil <anutorn@gmail.com>
2022-05-25 23:58:43 +0700

    Trying to assign players to characters

--- script/menu.gd
+++ script/menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var is_change_jump = false
-
+const PORT = 50021
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,3 +29,40 @@ func _on_Button_pressed():
 	is_change_jump = true
 	print("button pressed")
 
+
+func _on_Host_Button_pressed():
+	$ui.visible = false
+	var network = NetworkedMultiplayerENet.new()
+	network.create_server(PORT, 2)
+	get_tree().set_network_peer(network)
+	var is_server = get_tree().is_network_server()
+	print("is_server: ", is_server)
+	print(IP.get_local_addresses())
+
+	# connect signals
+	network.connect("peer_connected",self,"_peer_connected")
+	network.connect("peer_disconnected",self,"_peer_disconnected")
+
+
+func _on_Join_button_pressed():
+	var network = NetworkedMultiplayerENet.new()
+	network.create_client($ui/Ip_TextEdit.text, PORT)
+	get_tree().set_network_peer(network)
+	network.connect("connection_failed",self,"_on_connection_failed")
+	get_tree().multiplayer.connect("network_peer_packet",self,"_on_packet_received")
+	print(IP.get_local_addresses())
+
+
+func _on_connection_failed():
+	$ui/Status_Label.text = "Connect failed"
+
+
+func _peer_connected(id):
+#	$user_count_label.text = "Total Users:" + str(get_tree().get_network_connected_peers().size())
+	print("peer connected! ", id)
+
+  
+func _peer_disconnected(id):
+#	$user_count_label.text = "Total Users:" + str(get_tree().get_network_connected_peers().size())
+	print("peer disconnected!", id)
+	
--- script/p1.gd
+++ script/p1.gd
@@ -7,35 +7,40 @@ extends Node2D
 
 
 func _physics_process(delta):
-	if not $char.is_stunned:
-		if Input.is_action_pressed("p1_attack"):
-			$char.play_attack01()
-			
-		if Input.is_action_pressed("p1_up"):
-			$char.jump()
+	if is_network_master():
+		if not $char.is_stunned:
+			if Input.is_action_pressed("p1_attack"):
+				$char.play_attack01()
+				
+			if Input.is_action_pressed("p1_up"):
+				$char.jump()
 
-		# stop moving when both right and left are pressed
-		if Input.is_action_pressed("p1_right") and Input.is_action_pressed("p1_left"):
-			$char.reset_dash_trigger()
-			$char.stay_still()
-		elif Input.is_action_pressed("p1_right"):
-			$char.walk_right()
-		elif Input.is_action_pressed("p1_left"):
-			$char.walk_left()
-		elif Input.is_action_pressed("p1_right") == false:
-			$char.lerp_motion_x()
-		else:
-			$char.stay_still()
+			# stop moving when both right and left are pressed
+			if Input.is_action_pressed("p1_right") and Input.is_action_pressed("p1_left"):
+				$char.reset_dash_trigger()
+				$char.stay_still()
+			elif Input.is_action_pressed("p1_right"):
+				$char.walk_right()
+			elif Input.is_action_pressed("p1_left"):
+				$char.walk_left()
+			elif Input.is_action_pressed("p1_right") == false:
+				$char.lerp_motion_x()
+			else:
+				$char.stay_still()
 
-		# check dash right
-		if Input.is_action_just_pressed("p1_right"):
-			$char.dash_right()
-		elif Input.is_action_just_pressed("p1_left"):
-			$char.dash_left()
-		elif Input.is_action_just_released("p1_right"):
-			$char.set_run(false)
-		elif Input.is_action_just_released("p1_left"):
-			$char.set_run(false)
+			# check dash right
+			if Input.is_action_just_pressed("p1_right"):
+				$char.dash_right()
+			elif Input.is_action_just_pressed("p1_left"):
+				$char.dash_left()
+			elif Input.is_action_just_released("p1_right"):
+				$char.set_run(false)
+			elif Input.is_action_just_released("p1_left"):
+				$char.set_run(false)
 
-		if Input.is_action_just_pressed("p1_down"):
-			$char.downward_dash()
+			if Input.is_action_just_pressed("p1_down"):
+				$char.downward_dash()
+
+
+func _on_Network_tick_rate_timeout():
+	pass # Replace with function body.
